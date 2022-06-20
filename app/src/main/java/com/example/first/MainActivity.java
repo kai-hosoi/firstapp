1551cc94f3a17a9208a870dac51aa128cb8dae4b
kai-hosoi <79440232+kai-hosoi@users.noreply.github.com>
2022-06-20 23:32:40 +0900

    AsyncTaskサブクラスの作成

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.first;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startTask(View view) {
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        // Generate a random number between 0 and 10
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        // Make the task take long enough that we have
+        // time to rotate the phone while it is running
+        int s = n * 200;
+
+        // Sleep for the random amount of time
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Return a String result
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
     }
 }
 
