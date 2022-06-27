99f0e9934768c98feaa64df6bd0a442eb2f6f010
Hosoi-kai <hosoiki@sohobb.jp>
2022-06-27 18:31:22 +0900

    "Snackbarの実装試し"

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -5,41 +5,21 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    public void startTask(View view) {
+        findViewById(R.id.btn).setOnClickListener(this);
     }
 
     @Override
-    protected String doInBackground(Void... voids) {
-
-        // Generate a random number between 0 and 10
-        Random r = new Random();
-        int n = r.nextInt(11);
-
-        // Make the task take long enough that we have
-        // time to rotate the phone while it is running
-        int s = n * 200;
-
-        // Sleep for the random amount of time
-        try {
-            Thread.sleep(s);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Return a String result
-        return "Awake at last after sleeping for " + s + " milliseconds!";
-    }
-    protected void onPostExecute(String result) {
-        mTextView.get().setText(result);
+    public void onClick(View v) {
+        Snackbar.make(v, "ボタンが押されました", Snackbar.LENGTH_SHORT).show();
     }
 }
 
